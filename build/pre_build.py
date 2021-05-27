8abc28880ba7cd1ffd73de7a3ddb222df0ee8306
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-05-27 06:28:58 -0500

    More compact code

--- build/pre_build.py
+++ build/pre_build.py
@@ -3,15 +3,24 @@ import os
 Import("env")
 
 def before_build():
+    gen_range = range(0+3, 512+3)
     op = 'DIV'
+
+    genfile = os.path.join(env['PROJECT_SRC_DIR'], 'test_declares.g.hpp')
+    print(f'Generating {genfile}')
+    with open(genfile, 'w') as f:
+        for index in gen_range:
+            print(f'TEST_NATIVE_DIV({index});', file=f)
+            print(f'TEST_FAST_DIV({index});', file=f)
+
     genfile = os.path.join(env['PROJECT_SRC_DIR'], 'test.g.hpp')
     print(f'Generating {genfile}')
     with open(genfile, 'w') as f:
-        for index in range(768+3, 1024+3):
-            print(f'Serial.print(F("{op}_{index} "));', file=f)
-            print(f'testCheckSum(TEST_NATIVE_{op}({index}));', file=f)
+        for index in gen_range:
+            print(f'Serial.print("{op}_{index} ");', file=f)
+            print(f'testCheckSum(TEST_FUNC_NAME(Native, {index}));', file=f)
             print('Serial.print(" ");', file=f)
-            print(f'testCheckSum(TEST_FAST_{op}({index}));', file=f) 
+            print(f'testCheckSum(TEST_FUNC_NAME(ShiftMul, {index}));', file=f)
             print('Serial.println("");', file=f)
 
 before_build()
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,43 @@
 #include <Arduino.h>
 #include "fast_div.h"
 
-template <typename F>
-unsigned long elapsed_time(unsigned long iterations, F f)
+typedef unsigned long (*pCheckSumFunc)(unsigned long);
+void testCheckSum(pCheckSumFunc func)
 {
-    auto startTime = micros();
-
-    for (unsigned long loop=0; loop<iterations; ++loop)
-    {
-      f();
-    }
-
-    auto stopTime = micros();
-    return stopTime-startTime;
-}
-
-template <typename testFunc>
-void testCheckSum(testFunc func)
-{
-  unsigned long checkSum = 0;
   unsigned long iterations = INT8_MAX/16;
 
-  unsigned long testTime = elapsed_time(iterations, 
-    [&checkSum, &func]() { func(checkSum); }
-  );
-  Serial.print(checkSum); Serial.print(" "); Serial.print(testTime);      
+  auto startTime = micros();
+
+  unsigned long checkSum = 0;
+  for (unsigned long loop=0; loop<iterations; ++loop)
+  {
+    checkSum += func(checkSum);
+  }
+
+  auto stopTime = micros();
+  auto elapsed = stopTime-startTime;
+
+  Serial.print(checkSum); Serial.print(" "); Serial.print(elapsed);      
 }
 
-#define TEST_DIV(OPERATION, Div) \
-[](unsigned long &checkSum) \
+#define TEST_FUNC_NAME(Op, Div) testDiv ## Op ## Div
+
+#define TEST_DIV(OPERATION, Op, Div) \
+unsigned long TEST_FUNC_NAME(Op, Div)(unsigned long checkSum) \
 { \
   for (int16_t loop = -8192; loop < 8192; ++loop) \
   { \
     checkSum += OPERATION(loop, Div); \
   } \
+  return checkSum; \
 }
-#define NATIVE_DIV(Operand, Div) (Operand / Div)
-#define TEST_NATIVE_DIV(Div) TEST_DIV(NATIVE_DIV, Div)
-#define SHIFT_MUL_DIV(Operand, Div) FAST_DIV16(Operand, Div)
-#define TEST_FAST_DIV(Div) TEST_DIV(SHIFT_MUL_DIV, Div)
 
-#define TEST_MOD(OPERATION, Div)\
-[](unsigned long &checkSum) \
-{ \
-  for (uint16_t loop = 0; loop <8192; ++loop) \
-  { \
-    checkSum += OPERATION(loop, Div); \
-  } \
-}
-#define NATIVE_MOD(Operand, Div) (Operand % Div)
-#define TEST_NATIVE_MOD(Div) TEST_MOD(NATIVE_MOD, Div)
-#define SHIFT_MUL_MOD(Operand, Div) FAST_MOD16(Operand, Div)
-#define TEST_FAST_MOD(Div) TEST_MOD(SHIFT_MUL_MOD, Div)
+#define NATIVE_DIV(Operand, Div) (Operand / Div)
+#define TEST_NATIVE_DIV(Div) TEST_DIV(NATIVE_DIV, Native, Div)
+#define SHIFT_MUL_DIV(Operand, Div) FAST_DIV16(Operand, Div)
+#define TEST_FAST_DIV(Div) TEST_DIV(SHIFT_MUL_DIV, ShiftMul, Div)
+
+#include "test_declares.g.hpp"
 
 void setup() {
   Serial.begin(9600);
